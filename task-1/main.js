578012058849cf83240f6cc245eba6fa53d882bd
Fikirat Javadov <68659297+FikiratJavadov@users.noreply.github.com>
2022-06-28 20:20:47 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,22 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+  var input = result.input; // "5,2 4 7 12 9"
+  var arr = input.split(","); // ['5', '2 4 7 12 9']
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  const arr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr2.includes(arr[i])) {
+      arr2.push(arr[i]);
+    }
+  }
+
+  console.log(arr2.length);
 });
