8666d494b136baed74087d49d47704ddb67febca
Fikirat Javadov <68659297+FikiratJavadov@users.noreply.github.com>
2022-06-28 20:45:13 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -8,7 +8,7 @@ prompt.get("input", function (err, result) {
     Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
     */
   var input = result.input; // "5,2 4 7 12 9"
-  var arr = input.split(","); // ['5', '2 4 7 12 9']
+  var arr = input.split(" "); // ['5', '2 4 7 12 9']
 
   const arr2 = [];
 
